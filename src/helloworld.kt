2dc6f29e9968e624b513041e11beae91dbad1669
Justin Bee <jbee.appz@gmail.com>
2019-05-11 21:07:03 -0400

    playing around with kotlin

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -6,4 +6,23 @@ fun main(){
     println("please enter your name.....")
     var name = readLine()
     println("you entered $name")
+    println("please enter a number to add")
+    var num1= readLine()
+    println("please enter another number to add")
+    var num2 = readLine()
+    var x=0
+    var y=0
+    try{
+        x = num1!!.toInt()
+        y = num2!!.toInt()
+
+    }catch( nfe: NumberFormatException){
+
+    }
+    println("The result is "+ add(x, y))
 }
+
+fun add(x: Int, y: Int) = x + y
+
+
+
